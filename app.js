25b639c2acf5f0ef805c146921a6dd9fa806e02a
Alexandros <alexandr.kyriakou@gmail.com>
2022-04-02 22:07:50 +0300

    dragleave Event

--- app.js
+++ app.js
@@ -6,16 +6,40 @@ let draggedItem = null;
 for (let i = 0; i < list_items.length; i++) {
   const item = list_items[i];
 
-  item.addEventListener("dragstart", () => {
-    console.log("dragStart");
+  item.addEventListener("dragstart", function () {
     draggedItem = item;
+    setTimeout(function () {
+      item.style.display = "none";
+    }, 0);
   });
 
-  item.addEventListener("dragend", () => {
-    console.log("dragend");
+  item.addEventListener("dragend", function () {
+    setTimeout(function () {
+      item.style.display = "block";
+      draggedItem = null;
+    }, 0);
   });
 
   for (let j = 0; j < lists.length; j++) {
-    const list = lists[i];
+    const list = lists[j];
+
+    list.addEventListener("dragover", (e) => {
+      e.preventDefault();
+      list.style.backgroundColor = `var(--selected)`;
+    });
+
+    list.addEventListener("dragenter", (e) => {
+      e.preventDefault();
+      list.style.backgroundColor = `var(--selected)`;
+    });
+
+    list.addEventListener("dragleave", () => {
+      list.style.backgroundColor = `var(--lists-bg)`;
+    });
+
+    list.addEventListener("drop", () => {
+      list.append(draggedItem);
+      list.style.backgroundColor = `var(--lists-bg)`;
+    });
   }
 }
